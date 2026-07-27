957ec0b7733a3f0b0e5f976714e08eedc393c080
R3 Deploy <deploy@r3fitness.com.br>
2026-07-27 20:26:30 -0300

    Deploy build 2026-07-27_20-26

---
+++ .next/static/chunks/app/admin/(dashboard)/quick-start/page-24a1d9b74e7a4e10.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[616,1347],{5229:(e,s,n)=>{Promise.resolve().then(n.bind(n,45)),Promise.resolve().then(n.t.bind(n,2619,23))}},e=>{e.O(0,[2151,8441,1255,7358],()=>e(e.s=5229)),_N_E=e.O()}]);
